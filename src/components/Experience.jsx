94857c30bbdb75a99a88d1bb92f866b6a76245b0
Sourav Majumder <souravmajumderk99@gmail.com>
2023-11-01 23:35:06 +0600

    mesh and stage

---
+++ src/components/Experience.jsx
@@ -0,0 +1,41 @@
+import {
+  MeshReflectorMaterial,
+  PresentationControls,
+  Stage,
+} from "@react-three/drei";
+import React from "react";
+
+const Experience = () => {
+  return (
+    <PresentationControls
+      speed={1.5}
+      global
+      zoom={0.7}
+      polar={[-0.1, Math.PI / 4]}
+    >
+      <Stage environment={"city"} intensity={0.6} castShadow={false}>
+        <mesh>
+          <boxGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Stage>
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[170, 170]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={40}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color={"#a0a0a0"}
+          metalness={0.5}
+        />
+      </mesh>
+    </PresentationControls>
+  );
+};
+
+export default Experience;
