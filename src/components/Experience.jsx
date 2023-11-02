3558c7be9889b8584fcccf2a38e0147c95ca0b56
Sourav Majumder <souravmajumderk99@gmail.com>
2023-11-02 20:32:31 +0600

    new components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,38 +3,37 @@ import {
   PresentationControls,
   Stage,
 } from "@react-three/drei";
-import React from "react";
+import Chair from "./Chair";
 
 const Experience = () => {
   return (
-    <PresentationControls
-      speed={1.5}
-      global
-      zoom={0.7}
-      polar={[-0.1, Math.PI / 4]}
-    >
-      <Stage environment={"city"} intensity={0.6} castShadow={false}>
-        <mesh>
-          <boxGeometry />
-          <meshNormalMaterial />
+    <>
+      <PresentationControls
+        speed={1.5}
+        global
+        polar={[-0.1, Math.PI / 4]}
+        rotation={[Math.PI / 8, Math.PI / 4, 0]}
+      >
+        <Stage environment="city" intensity={0.6} castShadow={false}>
+          <Chair />
+        </Stage>
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-2}>
+          <planeGeometry args={[170, 170]} />
+          <MeshReflectorMaterial
+            blur={[300, 100]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={40}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#101010"
+            metalness={0.5}
+          />
         </mesh>
-      </Stage>
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[170, 170]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={40}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color={"#a0a0a0"}
-          metalness={0.5}
-        />
-      </mesh>
-    </PresentationControls>
+      </PresentationControls>
+    </>
   );
 };
 
